d3642bd90b6d28d2145e47447d70e2eee8dd6159
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-05-12 12:06:16 +1200

    send the necessary data to be used by pagination component from movies component as props, including the length of the movies array, the number of page size to be displayed and creates and includes a movies method that will handle this event when the page is clicked by the user

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -6,6 +6,7 @@ import { getMovies } from "../services/fakeMovieService";
 class Movies extends Component {
   state = {
     movies: getMovies(),
+    pageSize: 4
   };
 
   handleDelete = (movie) => {
@@ -19,6 +20,10 @@ class Movies extends Component {
     movies[index] = { ...movies[index] };
     movies[index].liked = !movies[index].liked;
     this.setState({ movies });
+  };
+
+  handlePageChange = (page) => {
+      console.log(page);
   };
 
   render() {
@@ -64,7 +69,7 @@ class Movies extends Component {
             ))}
           </tbody>
         </table>
-        <Pagination />
+        <Pagination itemsCount={count} pageSize={this.state.pageSize} onPageChange={this.handlePageChange}/>
       </>
     );
   }
