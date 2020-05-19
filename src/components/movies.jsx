0ed2110a755a35214c57222abef8943e4d220b6c
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-05-19 21:30:00 +1200

    receive the new sortColumn object sent by moviesTable method that manages the sorting and raise the result and update the state accordingly

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -43,14 +43,7 @@ class Movies extends Component {
     this.setState({ selectedGenre: genre, currentPage: 1 });
   };
 
-  handleSort = (path) => {
-    const sortColumn = { ...this.state.sortColumn };
-    if (sortColumn.path === path)
-      sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
-    else {
-      sortColumn.path = path;
-      sortColumn.order = "asc";
-    }
+  handleSort = (sortColumn) => {
     this.setState({ sortColumn });
   };
 
