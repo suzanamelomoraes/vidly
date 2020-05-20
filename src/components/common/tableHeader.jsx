f2d0a34ab667225a72198dd620da458ad65cda9d
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-05-20 20:55:10 +1200

    update the argument of raiseSort and the the content of the th tag to a reusable variables called columns that will make the component also reusable as well

--- src/components/common/tableHeader.jsx
+++ src/components/common/tableHeader.jsx
@@ -5,25 +5,27 @@ import React, { Component } from "react";
 // onSort: function
 
 class TableHeader extends Component {
+  raiseSort = (path) => {
+    const sortColumn = { ...this.props.sortColumn };
+    if (sortColumn.path === path)
+      sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
+    else {
+      sortColumn.path = path;
+      sortColumn.order = "asc";
+    }
+    this.props.onSort(sortColumn);
+  };
   render() {
-    raiseSort = (path) => {
-      const sortColumn = { ...this.props.sortColumn };
-      if (sortColumn.path === path)
-        sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
-      else {
-        sortColumn.path = path;
-        sortColumn.order = "asc";
-      }
-      this.props.onSort(sortColumn);
-    };
-    return(
-        <thead>
+    return (
+      <thead>
         <tr>
-    {this.props.columns.map(column => <th>{column.label}</th>)}
+          {this.props.columns.map((column) => (
+            <th onClick={() => this.raiseSort(column.path)}>{column.label}</th>
+          ))}
         </tr>
       </thead>
-    )
+    );
   }
-};
+}
 
 export default TableHeader;
