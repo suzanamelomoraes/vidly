59857aeb5adb6f973349a290bada5eacff8be8c6
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-05-20 21:28:17 +1200

    create a component to manage and display the body of the table from moviesTable component

---
+++ src/components/common/tableBody.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class tableBody extends Component {
+    render() {
+        return (
+            <tbody>
+                <tr>
+                    <td></td>
+                </tr>
+            </tbody>
+        )
+    }
+};
+
+export default tableBody;
