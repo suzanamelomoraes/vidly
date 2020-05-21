a320a50e24b4cbc3209e694de3557ed0d1190c56
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-05-21 15:44:46 +1200

    apply css class clickable on the th tag

--- src/components/common/tableHeader.jsx
+++ src/components/common/tableHeader.jsx
@@ -12,13 +12,13 @@ class TableHeader extends Component {
     this.props.onSort(sortColumn);
   };
 
-renderSortIcon = (column) => {
-    const { sortColumn } = this.props
+  renderSortIcon = (column) => {
+    const { sortColumn } = this.props;
 
-    if(column.path !== sortColumn.path) return null;
-    if(sortColumn.order === 'asc') return <i className='fa fa-sort-asc'></i>;
-    return <i className="fa fa-sort-desc"></i>
-};
+    if (column.path !== sortColumn.path) return null;
+    if (sortColumn.order === "asc") return <i className='fa fa-sort-asc'></i>;
+    return <i className='fa fa-sort-desc'></i>;
+  };
 
   render() {
     return (
@@ -26,6 +26,7 @@ renderSortIcon = (column) => {
         <tr>
           {this.props.columns.map((column) => (
             <th
+              className='clickable'
               key={column.path || column.key}
               onClick={() => this.raiseSort(column.path)}
             >
