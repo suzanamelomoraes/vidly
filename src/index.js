59cb8f75c73e215def078a43ed7b579cd4ed3837
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-06-08 18:34:47 +1200

    wrap App component with the browser router

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 serviceWorker.unregister();
