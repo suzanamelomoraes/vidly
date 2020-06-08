752b534cca186c516fc6b9d976dab6fc3d3561db
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-06-08 20:39:55 +1200

    apply exact propert to home path and redirect any other path to not-found

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ class App extends Component {
           <Route path='/customers' component={Customers}></Route>
           <Route path='/rentals' component={Rentals}></Route>
           <Route path='/not-found' component={NotFound}></Route>
-          <Redirect from='/' to='/movies' />
+          <Redirect from='/' exact to='/movies' />
+          <Redirect to='/not-found' />
         </Switch>
       </main>
     );
