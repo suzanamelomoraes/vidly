de2c3aedd3cef4838e22f7ed33ec486843bf31d9
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-06-08 20:59:41 +1200

    create a navbar component and insert a bootstrap navbar template, also import NavLink from react router and replace all the anchor to navlink and href tags to to

---
+++ src/components/navBar.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import NavLink from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <div>
+      <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+        <NavLink className='navbar-brand' to='#'>
+          Navbar
+        </NavLink>
+        <button
+          className='navbar-toggler'
+          type='button'
+          data-toggle='collapse'
+          data-target='#navbarSupportedContent'
+          aria-controls='navbarSupportedContent'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+        >
+          <span className='navbar-toggler-icon'></span>
+        </button>
+
+        <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+          <div className='navbar-nav mr-auto'>
+            <NavLink className='nav-item nav-link active' to='#'>
+              Home <span className='sr-only'>(current)</span>
+            </NavLink>
+            <NavLink className='nav-item nav-link' to='#'>
+              Link
+            </NavLink>
+            <NavLink className='nav-item nav-link' to='#'>
+              Link
+            </NavLink>
+            <NavLink
+              className='nav-item nav-link disabled'
+              to='#'
+              tabindex='-1'
+              aria-disabled='true'
+            >
+              Disabled
+            </NavLink>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
