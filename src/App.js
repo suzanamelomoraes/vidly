a6a0c335e66a3b9cebbbe4e9786d199c9276c178
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-06-08 20:35:23 +1200

    import redirect and switch from react router and apply it to redirect the home path to movies component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route } from "react-router-dom";
+import { Route, Redirect, Switch } from "react-router-dom";
 import Movies from "./components/movies";
 import Customers from "./components/customers";
 import Rentals from "./components/rentals";
@@ -10,10 +10,13 @@ class App extends Component {
   render() {
     return (
       <main className='container'>
-        <Route path='/movies' component={Movies}></Route>
-        <Route path='/customers' component={Customers}></Route>
-        <Route path='/rentals' component={Rentals}></Route>
-        <Route path='/not-found' component={NotFound}></Route>
+        <Switch>
+          <Route path='/movies' component={Movies}></Route>
+          <Route path='/customers' component={Customers}></Route>
+          <Route path='/rentals' component={Rentals}></Route>
+          <Route path='/not-found' component={NotFound}></Route>
+          <Redirect from='/' to='/movies' />
+        </Switch>
       </main>
     );
   }
