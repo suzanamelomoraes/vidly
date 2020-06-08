330b0f1c025f8d17badfb25394100a06d21064ae
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-06-08 20:11:04 +1200

    import route and customers, retals and not found components, also especify the routes to them

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from "react";
-import "./App.css";
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
 import Movies from "./components/movies";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import "./App.css";
 
-function App() {
-  return (
-    <main className="container">
-      <Movies />
-    </main>
-  );
+class App extends Component {
+  render() {
+    return (
+      <main className='container'>
+        <Route path='/movies' component={Movies}></Route>
+        <Route path='/customers' component={Customers}></Route>
+        <Route path='/rentals' component={Rentals}></Route>
+        <Route path='/not-found' component={NotFound}></Route>
+      </main>
+    );
+  }
 }
 
 export default App;
