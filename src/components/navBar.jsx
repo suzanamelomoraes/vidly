4b9012d5e07f825cf86a6683ebefc76f4d7fa8ef
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-06-08 22:09:25 +1200

    update navbar template to the newest one, moving the divs to a ul

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -3,38 +3,43 @@ import { Link, NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <div>
-      <nav className='navbar navbar-expand-lg navbar-light bg-light'>
-        <Link className='navbar-brand' to='/'>
-          Vidly
-        </Link>
-        <button
-          className='navbar-toggler'
-          type='button'
-          data-toggle='collapse'
-          data-target='#navbarSupportedContent'
-          aria-controls='navbarSupportedContent'
-          aria-expanded='false'
-          aria-label='Toggle navigation'
-        >
-          <span className='navbar-toggler-icon'></span>
-        </button>
+    <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+      <Link className='navbar-brand' to='/'>
+        Vidly
+      </Link>
 
-        <div className='collapse navbar-collapse' id='navbarSupportedContent'>
-          <div className='navbar-nav mr-auto'>
-            <NavLink className='nav-item nav-link' to='/movies'>
-              Movies <span className='sr-only'>(current)</span>
+      <button
+        className='navbar-toggler'
+        type='button'
+        data-toggle='collapse'
+        data-target='#navbarSupportedContent'
+        aria-controls='navbarSupportedContent'
+        aria-expanded='false'
+        aria-label='Toggle navigation'
+      >
+        <span className='navbar-toggler-icon'></span>
+      </button>
+
+      <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+        <ul className='navbar-nav mr-auto'>
+          <li className='nav-item'>
+            <NavLink className='nav-link' to='/movies'>
+              Movies
             </NavLink>
-            <NavLink className='nav-item nav-link' to='/customers'>
+          </li>
+          <li className='nav-item'>
+            <NavLink className='nav-link' to='/customers'>
               Customers
             </NavLink>
-            <NavLink className='nav-item nav-link' to='/rentals'>
+          </li>
+          <li className='nav-item'>
+            <NavLink className='nav-link' to='/rentals'>
               Rentals
             </NavLink>
-          </div>
-        </div>
-      </nav>
-    </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
