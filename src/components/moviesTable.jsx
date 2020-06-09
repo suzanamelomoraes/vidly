efec73cf2c7a36bd8e8d403f536e27653e1d31d5
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-06-09 21:49:54 +1200

    Import movieForm component to App.js and register a route with the path of movies/:id to this component

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -3,10 +3,15 @@ import { Link } from "react-router-dom";
 import Table from "./common/table";
 import Like from "./common/like";
 
-
 class MoviesTable extends Component {
   columns = [
-  { path: "title", label: "Title", content: movie => <Link to={`/movies/${movie._id}`}>{movie.title}</Link> },
+    {
+      path: "title",
+      label: "Title",
+      content: (movie) => (
+        <Link to={`/movies/${movie._id}`}>{movie.title}</Link>
+      ),
+    },
     { path: "genre.name", label: "Genre" },
     { path: "numberInStock", label: "Stock" },
     { path: "dailyRentalRate", label: "Rate" },
