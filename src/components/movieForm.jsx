84984b40924ec813a7328c22fa2fa79f749389a3
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-06-09 21:56:48 +1200

    Render the id of the movies by grabing it from params of the match property

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const MovieForm = () => {
-  return <h1>MovieForm</h1>;
+const MovieForm = ({match}) => {
+return <h1>MovieForm {match.params.id}</h1>;
 };
 
 export default MovieForm;
