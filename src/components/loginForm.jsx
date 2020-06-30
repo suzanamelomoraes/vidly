c1fddb30713953a317813824b861e86eb3b1473b
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-06-30 17:47:48 +1200

    include autofocus property

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -15,11 +15,21 @@ export class LoginForm extends Component {
         <form onSubmit={this.handleSubmit}>
           <div className='form-group'>
             <label htmlFor='username'>Username</label>
-            <input id='username' type='text' className='form-control' />
+            <input
+              autoFocus
+              id='username'
+              type='text'
+              className='form-control'
+            />
           </div>
           <div className='form-group'>
             <label htmlFor='password'>Password</label>
-            <input id='password' type='text' className='form-control' />
+            <input
+              autoFocus
+              id='password'
+              type='text'
+              className='form-control'
+            />
           </div>
           <button className='btn btn-primary'>Login</button>
         </form>
