b348f8747d325e24896898049a8e395ec3149101
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-07-22 18:32:37 +1200

    include value, name and onChange properties in the inputs and call the handleChange method in the onChange

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -15,6 +15,12 @@ export class LoginForm extends Component {
     console.log('Submitted');
   };
 
+  handleChange = (e) => {
+    const account = { ...this.state.account };
+    account.username = e.currentTarget.value;
+    this.setState({ account });
+  };
+
   render() {
     return (
       <div>
@@ -23,19 +29,25 @@ export class LoginForm extends Component {
           <div className='form-group'>
             <label htmlFor='username'>Username</label>
             <input
-              autoFocus
+              value={this.state.account.username}
+              onChange={this.handleChange}
               id='username'
+              name='username'
               type='text'
               className='form-control'
+              autoFocus
             />
           </div>
           <div className='form-group'>
             <label htmlFor='password'>Password</label>
             <input
-              autoFocus
+              value={this.state.account.password}
+              onChange={this.handleChange}
               id='password'
+              name='password'
               type='text'
               className='form-control'
+              autoFocus
             />
           </div>
           <button className='btn btn-primary'>Login</button>
